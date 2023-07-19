d2213c1baac5609cc025bd9c36abd4b2474e9a3e
ptp28 <parthparikh28@gmail.com>
2023-07-18 21:49:40 -0400

    Updated a few details

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
     * Start Bootstrap - Resume v6.0.0 (https://startbootstrap.com/template-overviews/resume)
-    * Copyright 2013-2022 Start Bootstrap
+    * Copyright 2013-2023 Start Bootstrap
     * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-resume/blob/master/LICENSE)
     */
     (function ($) {
